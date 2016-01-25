c1b367048486962c6e21f4a2338fbec3104009fd
CivilWolf <civilwo1f254@gmail.com>
2016-01-25 15:20:51 -0600

    Images and Cursors
    Added code for scrolling background and cursor images.

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,6 +4,7 @@
 #if defined(__APPLE__)
 
 #include "SDL2/SDL.h"
+#include "SDL2_image/SDL_image.h"
 
 #endif
 #if defined(__linux__)
@@ -20,11 +21,22 @@
 #include <iostream>
 using namespace std;
 
+//code for frame rate independence
+float deltaTime = 0.0;
+int thisTime = 0;
+int lastTime = 0;
+
+
+
+
+
+
+
 int main(int argc, char* argv[]) {
 
 	SDL_Window *window;                    // Declare a pointer
 
-	SDL_Init (SDL_INIT_EVERYTHING);              // Initialize SDL2
+	SDL_Init(SDL_INIT_EVERYTHING);              // Initialize SDL2
 
 	// Create an application window with the following settings:
 	window = SDL_CreateWindow("An SDL2 window",                  // window title
@@ -35,12 +47,98 @@ int main(int argc, char* argv[]) {
 			SDL_WINDOW_OPENGL                  // flags - see below
 			);
 
+
+#if defined(__APPLE__)
+	{
+		cout << "Running on Apple" << endl;
+		//get the current working directory
+		string s_cwd(getcwd(NULL,0));
+
+		//create a string linking to the mac's images folder
+		string s_cwd_images = s_cwd + "/Resources/images/";
+	}
+#endif
+
+
 	// Check that the window was successfully created
 	if (window == NULL) {
 		// In the case that the window could not be made...
 		printf("Could not create window: %s\n", SDL_GetError());
 		return 1;
 	}
+
+	SDL_Renderer*renderer = NULL;
+
+	//create renderer
+	renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
+
+	//CreateBackground
+	string BKGDpath = s_cwd_images + "/face.png";
+
+	//create a SDL surface to hold the background images
+	SDL_Surface*surface = IMG_Load(BKGDpath.c_str());
+
+	//create SDL_Texture
+	SDL_Texture*bkgd1;
+	//place surface info into the texture bkgd1;
+	bkgd1.CreateTextureFromSurface(renderer,surface);
+
+	//create SDL_Texture
+		SDL_Texture*bkgd2:
+		//place surface info into the texture bkgd1;
+		bkgd2.CreateTextureFromSurface(renderer,surface);
+
+	//free SDL surface
+	//SDL_FreeSurface(surface);
+
+	//Create the SDL_Rectangle for the texture's position and size -x,y,w,h
+	SDL_Rect bkgd1Pos;
+	//set the X,T,W, and H for the Rectangle
+	bkgd1Pos.x = 0;
+	bkgd1Pos.y = 0;
+	bkgd1Pos.w = 1024;
+	bkgd1Pos.h = 768;
+
+	//Create the SDL_Rectangle for the texture's position and size -x,y,w,h
+	SDL_Rect bkgd2Pos;
+	//set the X,T,W, and H for the Rectangle
+	bkgd2Pos.x = 0;
+	bkgd2Pos.y = -768;
+	bkgd2Pos.w = 1024;
+	bkgd2Pos.h = 768;
+
+
+
+	//Create the SDL_Rectangle for the texture's position and size -x,y,w,h
+		SDL_Rect cursorPos;
+		//set the X,T,W, and H for the Rectangle
+		cursorPos.x = 0;
+		cursorPos.y = 0;
+		cursorPos.w = 64;
+		cursorPos.h = 64;
+
+
+	//set temp variables to hold movement
+	float BG1pos_X = 0,BG1pos_Y = 0;
+
+	//set temp variables to hold movement
+	float BG2pos_X = 0,BG2pos_Y = -768;
+
+	//create cursor
+	string CURSORpath = s_cwd_images + "/Cursor.png";
+
+	surface = IMG_Load(CURSORpath.c_str());
+	SDL_Texture*cursor;
+	cursor= SDL_CreateTextureFromSurface(renderer,surface);
+
+
+
+
+
+	int bkgdSpeed = 100;
+
+/*
 	//////////////////////////////////////////////
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
@@ -54,6 +152,7 @@ int main(int argc, char* argv[]) {
 
 	//Update the surface
 	SDL_UpdateWindowSurface(window);
+	*/
 
 	//Setup a Game Controller Variable
 	SDL_GameController* gGameController = NULL;
@@ -84,6 +183,7 @@ int main(int argc, char* argv[]) {
 	while (!quit) {
 		switch (gameState) {
 		case MENU:
+		{
 			menu = true;
 			cout << "The Game State is Menu" << endl;
 			cout << "Press the A Button for Instructions" << endl;
@@ -92,6 +192,13 @@ int main(int argc, char* argv[]) {
 			cout << "Press the Y Button to Quit the Game" << endl;
 			cout << endl;
 			while (menu) {
+
+				//set up framerate for the section, or CASE
+				thisTime = SDL_GetTicks();
+				deltaTime=(float)(thisTime - lastTime)/1000;
+				lastTime = thisTime;
+
+
 				//check for input events
 				if (SDL_PollEvent(&event)) {
 					//check to see if the SDL Window is closed - player clicks X in the window
@@ -129,9 +236,57 @@ int main(int argc, char* argv[]) {
 						break;
 					}
 				}
-				break;
+
+				}
+				//Start Drawing
+
+			//Update
+			//Update Background 1
+			BG1pos_X += (bkgdSpeed *1)*deltaTime;
+			//Set the new bkgd1 position
+			bkgd1Pos.y = (int)(BG1pos_Y + 0.5f);
+
+			//reset when off the bottom of the screen
+			if(bkgd1Pos.y >= 768)
+			{
+				bkgd1Pos.y = -768;
+				BG1pos_Y = bkgd1Pos.Y;
+			}
+
+
+			//Update Background 2
+						BG2pos_X += (bkgdSpeed *1)*deltaTime;
+						//Set the new bkgd1 position
+						bkgd2Pos.y = (int)(BG2pos_Y + 0.5f);
+
+						//reset when off the bottom of the screen
+						if(bkgd2Pos.y >= 768)
+						{
+							bkgd2Pos.y = -768;
+							BG2pos_Y = bkgd2Pos.Y;
+						}
+
+			//Start Drawing
+				//Clear SDL Renderer
+				SDL_RenderClear(renderer);
+
+				//Draw the bkgd image
+				SDL_RenderCopy(renderer,bkgd1,NULL,&bkgd1Pos);
+				//Draw the bkgd image
+				SDL_RenderCopy(renderer,bkgd2,NULL,&bkgd1Pos);
+				/*
+				//Draw the bkgd image
+				SDL_RenderCopy(renderer,bkgd2,NULL,&bkgd1Pos);
+				*/
+
+				//SDL Render present
+				SDL_RenderPresent(renderer);
+
+				}
+				break;//End menu
 
 				case INSTRUCTIONS:
+				{
 				instructions = true;
 				cout << "The Game State is Instructions" << endl;
 				cout << "Press the A Button for Main Menu" << endl;
@@ -158,8 +313,11 @@ int main(int argc, char* argv[]) {
 							break;
 						}
 					}
+				}
+				}
 					break;	//end instructions case
 					case PLAYERS1:
+					{
 					players1 = true;
 					cout << "The Game State is Players 1" << endl;
 					cout << "Press the A Button for Win Screen" << endl;
@@ -194,8 +352,10 @@ int main(int argc, char* argv[]) {
 							}
 						}
 					}
+					}
 					break;	//end players1 case
 					case PLAYERS2:
+					{
 					players2 = true;
 					cout << "The Game State is Players 2" << endl;
 					cout << "Press the A Button for Win Screen" << endl;
@@ -230,8 +390,10 @@ int main(int argc, char* argv[]) {
 							}
 						}
 					}
+					}
 					break;	//end players2 case
 					case WIN:
+					{
 					win = true;
 					cout << "The Game State is Win" << endl;
 					cout << "Press the A Button for Main Menu" << endl;
@@ -266,8 +428,10 @@ int main(int argc, char* argv[]) {
 							}
 						}
 					}
+					}
 					break;	//end win case
 					case LOSE:
+					{
 					lose = true;
 					cout << "The Game State is Lose" << endl;
 					cout << "Press the A Button for Main Menu" << endl;
@@ -302,14 +466,11 @@ int main(int argc, char* argv[]) {
 							}
 						}
 					}
+					}
 					break;	//end win case
-
-				}
 
 			}
 		}
-
-	}
 
 	//SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
 
@@ -320,3 +481,4 @@ int main(int argc, char* argv[]) {
 	SDL_Quit();
 	return 0;
 }
+
